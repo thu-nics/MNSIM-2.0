07b0dc88bdb11cc835ded3a72c39f45506ebdc5a
ILTShade <sunhanbo123@163.com>
2019-10-22 14:51:16 +0800

    modify interface for multi-bit ReRAM

--- MNSIM/Interface/interface.py
+++ MNSIM/Interface/interface.py
@@ -2,119 +2,103 @@
 #-*-coding:utf-8-*-
 import math
 import numpy as np
-import quantize
 import torch
-import torch.nn as nn
 import collections
-from importlib import import_module
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--net', help = 'select net')
-parser.add_argument('-w', '--weight', help = 'select weight file')
-args = parser.parse_args()
-assert args.net
-assert args.weight
+import json
+import configparser
 
-# crossbar的尺寸，PE的尺寸指成对出现的crossbar数量，BANK尺寸指BANK二维大小
-crossbar_size = 128
-PE_size = 8
-BANK_size = (4, 4)
-
-# net
-net_module = import_module(args.net)
-activation_bit = net_module.activation_bit
-weight_bit = net_module.weight_bit
-net = net_module.get_net()
-net.load_state_dict(torch.load(args.weight))
-
-# transfer
-ASIC_ARRAY = []
-module_define = collections.OrderedDict()
-input_size = 32
-with torch.no_grad():
-    for name, module in net.named_modules():
-        if isinstance(module, nn.MaxPool2d):
-            output_size = input_size / 2
-        else:
-            output_size = input_size
-        if isinstance(module, quantize.QuantizeConv2d) or isinstance(module, quantize.QuantizePowerConv2d):
-            weight = module.conv2d.weight
-            scale = torch.max(torch.abs(weight))
-            thres = 2 ** (weight_bit - 1) - 1
-            weight.div_(scale).mul_(thres).round_()
-            weight = weight.reshape(weight.size(0), -1).t()
-            # layer_define
-            tmp_define = collections.OrderedDict()
-            tmp_define['Name'] = 'Conv'
-            tmp_define['Inputsize'] = input_size
-            tmp_define['Outputsize'] = output_size
-            tmp_define['Kernelsize'] = module.conv2d.kernel_size[0]
-            tmp_define['Stride'] = module.conv2d.stride[0]
-            tmp_define['Inputchannel'] = module.conv2d.in_channels
-            tmp_define['Outputchannel'] = module.conv2d.out_channels
-            tmp_define['Inputbit'] = activation_bit
-            tmp_define['Weightbit'] = weight_bit
-            tmp_define['Outputbit'] = activation_bit
-            module_define[name] = tmp_define
-            print('generate %s' % name)
-            # test
-            # weight = torch.arange(49.).reshape((7,7)) - 30
-            # weight_bit = 7
-            # split bit
-            weight_list = []
-            sign_weight = torch.sign(weight)
-            value_weight = torch.abs(weight)
-            for i in range(weight_bit - 1):
-                weight_list.append(torch.fmod(value_weight, 2))
-                value_weight.div_(2).floor_()
-            # add sign
-            sign_weight_list = []
-            for tmp in weight_list:
-                positive = torch.mul(tmp, (sign_weight == 1).float())
-                negative = torch.mul(tmp, (sign_weight == -1).float())
-                sign_weight_list.append([positive, negative])
-            # split channel
-            H = weight.size(0)
-            W = weight.size(1)
-            H_range = math.ceil(H / (BANK_size[0] * crossbar_size))
-            W_range = math.ceil(W / (BANK_size[1] * crossbar_size))
-            CrossArray = []
-            for h in range(H_range):
-                for w in range(W_range):
-                    base_h = h * BANK_size[0] * crossbar_size
-                    base_w = w * BANK_size[1] * crossbar_size
-                    total_h = min(H, (h+1)*BANK_size[0]*crossbar_size) - base_h
-                    total_w = min(W, (w+1)*BANK_size[1]*crossbar_size) - base_w
-                    # BANK内部
-                    split_h_range = math.ceil(total_h/crossbar_size)
-                    split_w_range = math.ceil(total_w/crossbar_size)
-                    for split_h in range(split_h_range):
-                        for split_w in range(split_w_range):
-                            PE_array = []
-                            split_base_h = base_h + split_h*crossbar_size
-                            split_base_w = base_w + split_w*crossbar_size
-                            total_split_h = min(H, split_base_h+crossbar_size) - split_base_h
-                            total_split_w = min(W, split_base_w+crossbar_size) - split_base_w
-                            # 写入新的矩阵
-                            for i in range(weight_bit - 1):
-                                tmp_positive = sign_weight_list[i][0][split_base_h:(split_base_h+total_split_h),split_base_w:(split_base_w+total_split_w)].clone()
-                                tmp_negative = sign_weight_list[i][1][split_base_h:(split_base_h+total_split_h),split_base_w:(split_base_w+total_split_w)].clone()
-                                PE_array.append([tmp_positive.numpy().astype(np.uint8), tmp_negative.numpy().astype(np.uint8)])
-                            CrossArray.append(PE_array)
-            A = math.ceil(len(CrossArray) / (BANK_size[0]*BANK_size[1]))
-            print(A)
-            for i in range(A):
-                BANK_array = []
-                for j in range(BANK_size[0]):
-                    ROW_array = []
-                    for k in range(BANK_size[1]):
-                        index = i * (BANK_size[0]*BANK_size[1]) + j*(BANK_size[1])+k
-                        if index > len(CrossArray) - 1:
-                            continue
-                        ROW_array.append(CrossArray[index])
-                    BANK_array.append(ROW_array)
-                ASIC_ARRAY.append(BANK_array)
-        input_size = output_size
-
-    torch.save(ASIC_ARRAY, './zoo/mnsim_weight.pt')
-    torch.save(module_define, './zoo/mnsim_net.pt')
+class TrainInterface(object):
+    def __init__(self, SimConfig_path, net_superparams_path, net_weights_path):
+        # config parser
+        xbar_config = configparser.ConfigParser()
+        xbar_config.read(SimConfig_path, encoding = 'UTF-8')
+        self.xbar_size = list(map(int, xbar_config.get('Crossbar level', 'Xbar_Size').split(',')))
+        self.xbar_row = self.xbar_size[0]
+        self.xbar_column = self.xbar_size[1]
+        self.xbar_bit = 1
+        self.pe_group_num = int(xbar_config.get('Process element level', 'Group_Num'))
+        self.bank_size = list(map(int, xbar_config.get('Bank level', 'PE_Num').split(',')))
+        self.bank_row = self.bank_size[0]
+        self.bank_column = self.bank_size[1]
+        # net super params load and weights load
+        f = open(net_superparams_path)
+        self.net_superparams = json.loads(f.read())
+        f.close()
+        self.net_weights = torch.load(net_weights_path)
+        self.__deploy_params = []
+        self.__deploy_flag = False
+    def get_structure(self):
+        if not self.__deploy_flag:
+            self.__generate_structure()
+        return self.__deploy_params
+    def __generate_structure(self):
+        for layer_param in self.net_superparams:
+            # read weight and check weight shape
+            layer_weight = self.net_weights[layer_param['Weightname']].detach().cpu()
+            assert layer_weight.shape[0] == layer_param['Outputchannel']
+            assert layer_weight.shape[1] == layer_param['Inputchannel']
+            assert layer_weight.shape[2] == layer_param['Kernelsize']
+            assert layer_weight.shape[3] == layer_param['Kernelsize']
+            # change float weight to fix point weight
+            scale = torch.max(torch.abs(layer_weight))
+            thres = 2 ** (layer_param['Weightbit'] - 1) - 1
+            layer_weight.div_(scale).mul_(thres).round_()
+            layer_weight = layer_weight.reshape(layer_weight.size(0), -1).t()
+            # fix point weight to split weight
+            sign_weight = torch.sign(layer_weight)
+            sign_positive = (sign_weight == 1).float()
+            sign_negative = (sign_weight == -1).float()
+            abs_weight = torch.abs(layer_weight)
+            pn_weight_list = []
+            assert (layer_param['Weightbit'] - 1) % self.xbar_bit == 0
+            bit_base = 2 ** (self.xbar_bit)
+            for i in range((layer_param['Weightbit'] - 1) // self.xbar_bit):
+                tmp = torch.fmod(abs_weight, bit_base)
+                positive = torch.mul(tmp, sign_positive)
+                negative = torch.mul(tmp, sign_negative)
+                pn_weight_list.append([positive, negative])
+                abs_weight.div_(bit_base).floor_()
+            # split channel for large input or output channel
+            weight_h = layer_weight.shape[0]
+            weight_w = layer_weight.shape[1]
+            h_range = math.ceil(weight_h / (self.bank_row * self.xbar_row))
+            w_range = math.ceil(weight_w / (self.bank_column * self.xbar_column))
+            xbar_array = []
+            for h in range(h_range):
+                for w in range(w_range):
+                    base_h = h * self.bank_row * self.xbar_row
+                    base_w = w * self.bank_column * self.xbar_column
+                    total_h = min(weight_h, (h + 1) * self.bank_row * self.xbar_row) - base_h
+                    total_w = min(weight_w, (w + 1) * self.bank_column * self.xbar_column) - base_w
+                    # split in bank
+                    bank_h_range = math.ceil(total_h / self.xbar_row)
+                    bank_w_range = math.ceil(total_w / self.xbar_column)
+                    for bank_h in range(bank_h_range):
+                        for bank_w in range(bank_w_range):
+                            bank_base_h = base_h + bank_h * self.xbar_row
+                            bank_base_w = base_w + bank_w * self.xbar_column
+                            bank_total_h = min(weight_h, bank_base_h + self.xbar_row) - bank_base_h
+                            bank_total_w = min(weight_w, bank_base_w + self.xbar_column) - bank_base_w
+                            # copy weight data to pe array
+                            pe_array = []
+                            for i in range((layer_param['Weightbit'] - 1) // self.xbar_bit):
+                                tmp_positive = pn_weight_list[i][0][bank_base_h : (bank_base_h + bank_total_h), bank_base_w : (bank_base_w + bank_total_w)].clone()
+                                tmp_negative = pn_weight_list[i][1][bank_base_h : (bank_base_h + bank_total_h), bank_base_w : (bank_base_w + bank_total_w)].clone()
+                                pe_array.append([tmp_positive.numpy().astype(np.uint8), tmp_negative.numpy().astype(np.uint8)])
+                            # copy data to cross array
+                            xbar_array.append(pe_array)
+            # rearrange for pe bank
+            L = math.ceil(len(xbar_array) / (self.bank_row * self.bank_column))
+            for i in range(L):
+                bank_array = []
+                for h in range(self.bank_row):
+                    for w in range(self.bank_column):
+                        serial_number = i * self.bank_row * self.bank_column + h * self.bank_column + w
+                        if serial_number < len(xbar_array):
+                            bank_array.append(xbar_array[serial_number])
+                # save for every bank array
+                layer_info = collections.OrderedDict()
+                for key in ['Layernum', 'Inputsize', 'Outputsize', 'Kernelsize', 'Stride', 'Inputchannel', 'Outputchannel', 'Inputbit', 'Weightbit', 'Outputbit']:
+                    layer_info[key] = layer_param[key]
+                self.__deploy_params.append((layer_info, bank_array))
+        self.__deploy_flag = True
