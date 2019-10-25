b2e9b2f821e9916c74576e743f05d14796a0c36d
ILTShade <sunhanbo123@163.com>
2019-10-25 12:23:47 +0800

    add interface message

--- MNSIM/Interface/interface.py
+++ MNSIM/Interface/interface.py
@@ -3,18 +3,42 @@ import math
 import numpy as np
 import torch
 import collections
-import json
 import configparser
 from importlib import import_module
 
 class TrainTestInterface(object):
-    def __init__(self, netwotk_module, dataset_module, weights_file, device = None):
+    def __init__(self, netwotk_module, dataset_module, SimConfig_path, weights_file, device = None):
         # netwotk_module = 'lenet'
         # dataset_module = 'cifar10'
         # weights_file = './zoo/cifar10_lenet_train_params.pth'
         # load net, dataset, and weights
-        self.net = import_module(netwotk_module).get_net()
         self.test_loader = import_module(dataset_module).get_dataloader()[1]
+        # load simconfig
+        ## xbar_size, input_bit, weight_bit, quantize_bit
+        xbar_config = configparser.ConfigParser()
+        xbar_config.read(SimConfig_path, encoding = 'UTF-8')
+        self.hardware_config = collections.OrderedDict()
+        self.hardware_config['fix_method'] = 'SINGLE_FIX_TEST'
+        # xbar_size
+        xbar_size = list(map(int, xbar_config.get('Crossbar level', 'Xbar_Size').split(',')))
+        self.xbar_row = xbar_size[0]
+        self.xbar_column = xbar_size[1]
+        self.hardware_config['xbar_size'] = xbar_size[0]
+        # xbar bit
+        self.xbar_bit = int(xbar_config.get('Device level', 'Device_Level'))
+        self.hardware_config['weight_bit'] = self.xbar_bit
+        # input bit and ADC bit
+        self.input_bit = 2
+        self.quantize_bit = 10
+        self.hardware_config['input_bit'] = self.input_bit
+        self.hardware_config['quantize_bit'] = self.quantize_bit
+        # group num
+        self.pe_group_num = int(xbar_config.get('Process element level', 'Group_Num'))
+        self.bank_size = list(map(int, xbar_config.get('Bank level', 'PE_Num').split(',')))
+        self.bank_row = self.bank_size[0]
+        self.bank_column = self.bank_size[1]
+        # net and weights
+        self.net = import_module(netwotk_module).get_net(self.hardware_config)
         self.net.load_state_dict(torch.load(weights_file))
         if device != None:
             self.device = torch.device(f'cuda:{device}' if torch.cuda.is_available() else 'cpu')
@@ -56,7 +80,3 @@ class TrainTestInterface(object):
 
 if __name__ == '__main__':
     pass
-    # inter = TrainTestInterface()
-    # print(inter.origin_evaluate())
-    # net_bit_weights = inter.get_bits()
-    # print(inter.set_bit_evaluate(net_bit_weights))
