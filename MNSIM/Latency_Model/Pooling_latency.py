e03c66dcd7f4d91c836248d2ae5eeb12921402b7
Zhu-Zhenhua <zhu-z14@outlook.com>
2019-11-18 22:58:32 +0800

    Update Pooling latency model

--- MNSIM/Latency_Model/Pooling_latency.py
+++ MNSIM/Latency_Model/Pooling_latency.py
@@ -7,23 +7,13 @@ import math
 import configparser as cp
 work_path = os.path.dirname(os.getcwd())
 sys.path.append(work_path)
-from MNSIM.Hardware_Model import *
-from MNSIM.Hardware_Model.Crossbar import crossbar
-from MNSIM.Hardware_Model.PE import ProcessElement
 from MNSIM.Hardware_Model.Buffer import buffer
-from MNSIM.Hardware_Model.Bank import bank
 from MNSIM.Interface.interface import *
-from MNSIM.Mapping_Model.Bank_connection_graph import BCG
-import collections
 
 class Pooling_latency_analysis():
     def __init__(self, SimConfig_path, indata=0, rdata=0):
-        # read_row: activated WL number in crossbar
-        # read_column: activated BL number in crossbar
-        # indata: volume of input data (for PE) (Byte)
+        # indata: volume of input data (for pooling) (Byte)
         # rdata: volume of data from buffer to iReg (Byte)
-        # outdata: volume of output data (for PE) (Byte)
-        # inprecision: input data precision of each Xbar
         Poolingl_config = cp.ConfigParser()
         Poolingl_config.read(SimConfig_path, encoding='UTF-8')
         self.buf = buffer(SimConfig_path)
