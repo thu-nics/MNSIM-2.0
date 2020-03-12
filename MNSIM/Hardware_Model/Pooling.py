2446810e9fb150a7491704688f7483795cbc271b
KendrickQ <2361648314@qq.com>
2020-03-12 17:16:26 +0800

    update the pooling part in config file

--- MNSIM/Hardware_Model/Pooling.py
+++ MNSIM/Hardware_Model/Pooling.py
@@ -9,22 +9,22 @@ class Pooling(object):
     def __init__(self, SimConfig_path):
         Pooling_config = cp.ConfigParser()
         Pooling_config.read(SimConfig_path, encoding='UTF-8')
-        self.Pooling_choice = Pooling_config.get()
+        # self.Pooling_choice = Pooling_config.get()
 
-        self.Poolilng_unit_num = int(Pooling_config.get(''))
-        if self.Poolilng_unit_num == 0:
+        self.Pooling_unit_num = int(Pooling_config.get('Tile level', 'Pooling_unit_num'))
+        if self.Pooling_unit_num == 0:
             self.Pooling_unit_num = 64
-        self.Pooling_Tech = int(Pooling_config.get())
+        self.Pooling_Tech = int(Pooling_config.get('Tile level', 'Pooling_Tech'))
         if self.Pooling_Tech == 0:
             self.Pooling_Tech = 65
-        self.Pooling_area = float(Pooling_config.get())
+        self.Pooling_area = float(Pooling_config.get('Tile level', 'Pooling_area'))
 
         self.Pooling_power = 0
         self.Pooling_energy = 0
         self.Pooling_area = 0
         self.Pooling_latency = 0
 
-        self.Pooling_shape = list(map(int, Pooling_config.get('Interface level', 'ADC_Interval_Thres').split(',')))
+        self.Pooling_shape = list(map(int, Pooling_config.get('Tile level', 'Pooling_shape').split(',')))
         assert len(self.Pooling_shape) == 2, "Input of Pooling shape is illegal"
         self.Pooling_size = self.Pooling_shape[0] * self.Pooling_shape[1]
         if self.Pooling_size == 0:
@@ -78,10 +78,10 @@ class Pooling(object):
         self.Pooling_energy = self.Pooling_power * self.Pooling_latency
 
     def Pooling_output(self):
-        if self.Pooling_choice == -1:
-            print("ADC_choice: User defined")
-        else:
-            print("default configuration")
+        # if self.Pooling_choice == -1:
+        #     print("ADC_choice: User defined")
+        # else:
+        #     print("default configuration")
         print("Pooling_shape: %d x %d" %(self.Pooling_shape[0], self.Pooling_shape[1]))
         print("Pooling_area:", self.Pooling_area, "um^2")
         print("Pooling_power:", self.Pooling_power, "W")
@@ -90,13 +90,14 @@ class Pooling(object):
 
 
 def Pooling_test():
-    print("load file:",test_SimConfig_path)
+    print("load file:", test_SimConfig_path)
     _Pooling = Pooling(test_SimConfig_path)
     _Pooling.calculate_Pooling_area()
     _Pooling.calculate_Pooling_power()
     _Pooling.calculate_Pooling_latency()
     _Pooling.calculate_Pooling_energy()
     _Pooling.Pooling_output()
+
 
 if __name__ == '__main__':
     Pooling_test()
