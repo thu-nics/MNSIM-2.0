3e56d03cce414ff4b1dfac3e09de67a02faa1a8a
Zhu-Zhenhua <pigzhu19960409@qq.com>
2021-08-17 14:00:00 +0800

    add utilization estimation (in Area model)

--- MNSIM/Area_Model/Model_Area.py
+++ MNSIM/Area_Model/Model_Area.py
@@ -12,6 +12,7 @@ import pandas as pd
 from MNSIM.Hardware_Model.Tile import tile
 from MNSIM.Hardware_Model.Buffer import buffer
 from MNSIM.Hardware_Model.Adder import adder
+import math
 class Model_area():
     def __init__(self, NetStruct, SimConfig_path, multiple=None, TCG_mapping=None):
         self.NetStruct = NetStruct
@@ -53,6 +54,12 @@ class Model_area():
         self.arch_total_buf_area = 0
         self.arch_total_output_mux_area = 0
         self.arch_total_pooling_area = 0
+        self.arch_xbar_utilization = self.total_layer_num * [0]
+        self.arch_DAC_utilization = self.total_layer_num * [0]
+        self.arch_ADC_utilization = self.total_layer_num * [0]
+        self.arch_total_xbar_utilization = 0
+        self.arch_total_DAC_utilization = 0
+        self.arch_total_ADC_utilization = 0
         # print(data.columns)
         # if NoC_Compute == 1:
         #     path = os.getcwd() + '/Final_Results/'
@@ -71,7 +78,17 @@ class Model_area():
         self.global_buf.calculate_buf_area()
         self.global_add = adder(SimConfig_path=self.SimConfig_path,bitwidth=self.graph.global_adder_bitwidth)
         self.global_add.calculate_adder_area()
+        self.tile = tile(SimConfig_path=self.SimConfig_path)
+        self.tile_xbar_num = self.tile.tile_PE_total_num*self.tile.group_num*self.tile.xbar_column*self.tile.xbar_row
+        self.tile_DAC_num = self.tile.tile_PE_total_num*self.tile.group_num*self.tile.xbar_row
+        self.tile_ADC_num = self.tile.tile_PE_total_num*self.tile.group_num*self.tile.xbar_column
+        total_tile_num = 0
+        used_total_xbar_num = 0
+        used_total_DAC_num = 0
+        used_total_ADC_num = 0
+        # not the real DAC/ADC num, but it reflects the DAC/ADC num
         for i in range(self.total_layer_num):
+            layer_dict = self.NetStruct[i][0][0]
             tile_num = self.graph.layer_tileinfo[i]['tilenum']
             self.arch_area[i] = self.graph.tile.tile_area * tile_num
             self.arch_xbar_area[i] = self.graph.tile.tile_xbar_area * tile_num
@@ -87,6 +104,30 @@ class Model_area():
             self.arch_jointmodule_area[i] = self.graph.tile.tile_jointmodule_area * tile_num
             self.arch_buf_area[i] = self.graph.tile.tile_buffer_area * tile_num
             self.arch_pooling_area[i] = self.graph.tile.tile_pooling_area * tile_num
+            if self.graph.layer_tileinfo[i]['type'] == 'conv':
+            # only consider the utilization rate of conv layer and fc layer
+                total_tile_num += tile_num
+                used_xbar_num = self.graph.layer_tileinfo[i]['x_width']*self.graph.layer_tileinfo[i]['y_height']
+                used_DAC_num = self.graph.layer_tileinfo[i]['y_height']*self.graph.layer_tileinfo[i]['weight_precision']*math.ceil(int(layer_dict['Outputchannel']) / self.tile.xbar_column)
+                used_ADC_num = self.graph.layer_tileinfo[i]['x_width']*self.graph.layer_tileinfo[i]['my']
+                self.arch_xbar_utilization[i] = used_xbar_num/(tile_num*self.tile_xbar_num)
+                self.arch_DAC_utilization[i] = used_DAC_num/(tile_num*self.tile_DAC_num)
+                self.arch_ADC_utilization[i] = used_ADC_num/(tile_num*self.tile_ADC_num)
+                used_total_xbar_num += used_xbar_num
+                used_total_DAC_num += used_DAC_num
+                used_total_ADC_num += used_ADC_num
+            if self.graph.layer_tileinfo[i]['type'] == 'fc':
+            # only consider the utilization rate of conv layer and fc layer
+                total_tile_num += tile_num
+                used_xbar_num = self.graph.layer_tileinfo[i]['x_width']*self.graph.layer_tileinfo[i]['y_height']
+                used_DAC_num = self.graph.layer_tileinfo[i]['y_height']*self.graph.layer_tileinfo[i]['weight_precision']*math.ceil(int(layer_dict['Outfeature']) / self.tile.xbar_column)
+                used_ADC_num = self.graph.layer_tileinfo[i]['x_width']*self.graph.layer_tileinfo[i]['my']
+                self.arch_xbar_utilization[i] = used_xbar_num/(tile_num*self.tile_xbar_num)
+                self.arch_DAC_utilization[i] = used_DAC_num/(tile_num*self.tile_DAC_num)
+                self.arch_ADC_utilization[i] = used_ADC_num/(tile_num*self.tile_ADC_num)
+                used_total_xbar_num += used_xbar_num
+                used_total_DAC_num += used_DAC_num
+                used_total_ADC_num += used_ADC_num
         self.arch_total_area = sum(self.arch_area)
         self.arch_total_xbar_area = sum(self.arch_xbar_area)
         self.arch_total_ADC_area = sum(self.arch_ADC_area)
@@ -101,6 +142,9 @@ class Model_area():
         self.arch_total_jointmodule_area = sum(self.arch_jointmodule_area)
         self.arch_total_buf_area = sum(self.arch_buf_area)+self.global_buf.buf_area
         self.arch_total_pooling_area = sum(self.arch_pooling_area)
+        self.arch_total_xbar_utilization = used_total_xbar_num/(total_tile_num*self.tile_xbar_num)
+        self.arch_total_DAC_utilization = used_total_DAC_num/(total_tile_num*self.tile_DAC_num)
+        self.arch_total_ADC_utilization = used_total_ADC_num/(total_tile_num*self.tile_ADC_num)
 
     def model_area_output(self, module_information = 1, layer_information = 1):
         print("Hardware area:", self.arch_total_area, "um^2")
@@ -118,6 +162,9 @@ class Model_area():
             print("			|---input_demux area:", self.arch_total_input_demux_area, "um^2")
             print("			|---output_mux area:", self.arch_total_output_mux_area, "um^2")
             print("			|---joint_module area:", self.arch_total_jointmodule_area, "um^2")
+            print("		crossbar utilization rate: ", self.arch_total_xbar_utilization*100, "%",sep='')
+            print("		DAC utilization rate: ", self.arch_total_DAC_utilization*100, "%",sep='')
+            print("		ADC utilization rate: ", self.arch_total_ADC_utilization*100, "%",sep='')
             # print("		NoC part area:", self.arch_Noc_area, "um^2")
         if layer_information:
             for i in range(self.total_layer_num):
@@ -127,6 +174,10 @@ class Model_area():
                     print("     Hardware area (global accumulator):", self.global_add.adder_area*self.graph.global_adder_num+self.global_buf.buf_area, "um^2")
                 else:
                     print("     Hardware area:", self.arch_area[i], "um^2")
+                if layer_dict['type'] == 'conv' or layer_dict['type'] == 'fc':
+                    print("		crossbar utilization rate: ", self.arch_xbar_utilization[i]*100, "%",sep='')
+                    print("		DAC utilization rate: ", self.arch_DAC_utilization[i]*100, "%",sep='')
+                    print("		ADC utilization rate: ", self.arch_ADC_utilization[i]*100, "%",sep='')
 
 if __name__ == '__main__':
     test_SimConfig_path = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "SimConfig.ini")
