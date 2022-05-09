303a9a368a56cf53b07957190c512554268d136b
ILTShade <sunhanbo123@163.com>
2022-05-09 09:49:05 +0000

    add source_tile_id list and true id in data format

--- MNSIM/Interface/evaluation.py
+++ MNSIM/Interface/evaluation.py
@@ -151,7 +151,10 @@ class EvaluationInterface(Component):
                             int(np.sum(output_channel_list[:j*self.hardware_config["tile_column"]])),
                             int(np.sum(output_channel_list[:(j+1)*self.hardware_config["tile_column"]]))
                         )
-                        tile_flag_list.append((out_channel_index[0], out_channel_index[1], len(tile_flag_list)))
+                        tile_flag_list.append((
+                            out_channel_index[0], out_channel_index[1],
+                            len(tile_flag_list), len(tile_behavior_list) + len(tile_flag_list)
+                        ))
                         column = out_channel_index[1] - out_channel_index[0]
                         # add transfer time
                         H_level = math.ceil(math.log2(PE_latency_slice.shape[0]))
@@ -171,6 +174,7 @@ class EvaluationInterface(Component):
                     tile_behavior["layer_id"] = layer_id
                     tile_behavior["tile_id"] = len(tile_behavior_list)
                     tile_behavior["target_tile_id"] = [merge_node_id]
+                    tile_behavior["source_tile_id"] = []
                     tile_behavior["merge_flag"] = False
                     dependence = [dict()
                         for _ in range(key_layer_info["Outputsize"][0]*key_layer_info["Outputsize"][1])
@@ -188,13 +192,13 @@ class EvaluationInterface(Component):
                             dependence[i*key_layer_info["Outputsize"][1] + j]["wait"] = [[
                                 wait_row, wait_column, 0, key_layer_info["Inputchannel"], \
                                 key_layer_info["Inputbit"], key_layer_info["Inputchannel"],
-                                None, None, -1
+                                None, None, -1, None
                             ]]
                             # output
                             dependence[i*key_layer_info["Outputsize"][1] + j]["output"] = [[
                                 i, j, tile_info[2][0], tile_info[2][1],
                                 key_layer_info["outputbit"], key_layer_info["Outputchannel"],
-                                None, layer_id, tile_id
+                                None, layer_id, tile_id, tile_behavior["tile_id"]
                             ]]
                             # latency
                             dependence[i*key_layer_info["Outputsize"][1] + j]["latency"] = float(tile_info[1])
@@ -211,7 +215,7 @@ class EvaluationInterface(Component):
                             dependence[drop_row*key_layer_info["Outputsize"][1] + drop_column]["drop"] = [[
                                 i, j, 0, key_layer_info["Inputchannel"], \
                                 key_layer_info["Inputbit"], key_layer_info["Inputchannel"],
-                                None, None, -1
+                                None, None, -1, None
                             ]] + origin_drop
                     tile_behavior["dependence"] = dependence
                     tile_behavior_list.append(tile_behavior)
@@ -221,6 +225,7 @@ class EvaluationInterface(Component):
                 tile_behavior["layer_id"] = layer_id
                 tile_behavior["tile_id"] = merge_node_id
                 tile_behavior["target_tile_id"] = []
+                tile_behavior["source_tile_id"] = [true_id for _, _, _, true_id in tile_flag_list]
                 tile_behavior["merge_flag"] = True
                 dependence = [dict()
                     for _ in range(key_layer_info["Outputsize"][0]*key_layer_info["Outputsize"][1])
@@ -232,13 +237,13 @@ class EvaluationInterface(Component):
                         dependence[i*key_layer_info["Outputsize"][1] + j]["wait"] = [[
                             i, j, start, end,
                             key_layer_info["outputbit"], key_layer_info["Outputchannel"],
-                            None, layer_id, tile_id
-                        ] for start, end, tile_id in tile_flag_list]
+                            None, layer_id, tile_id, true_id
+                        ] for start, end, tile_id, true_id in tile_flag_list]
                         # output
                         dependence[i*key_layer_info["Outputsize"][1] + j]["output"] = [[
                             i, j, 0, key_layer_info["Outputchannel"],
                             key_layer_info["outputbit"], key_layer_info["Outputchannel"],
-                            None, layer_id, -1
+                            None, layer_id, -1, tile_behavior["tile_id"]
                         ]]
                         # latency
                         dependence[i*key_layer_info["Outputsize"][1] + j]["latency"] = 1
@@ -297,7 +302,10 @@ class EvaluationInterface(Component):
                             int(np.sum(output_feature_list[:j*self.hardware_config["tile_column"]])),
                             int(np.sum(output_feature_list[:(j+1)*self.hardware_config["tile_column"]]))
                         )
-                        tile_flag_list.append((out_feature_index[0], out_feature_index[1], len(tile_flag_list)))
+                        tile_flag_list.append((
+                            out_feature_index[0], out_feature_index[1],
+                            len(tile_flag_list), len(tile_behavior_list) + len(tile_flag_list)
+                        ))
                         column = out_feature_index[1] - out_feature_index[0]
                         # add transfer time
                         H_level = math.ceil(math.log2(PE_latency_slice.shape[0]))
@@ -317,18 +325,19 @@ class EvaluationInterface(Component):
                     tile_behavior["layer_id"] = layer_id
                     tile_behavior["tile_id"] = len(tile_behavior_list)
                     tile_behavior["target_tile_id"] = [merge_node_id]
+                    tile_behavior["source_tile_id"] = []
                     tile_behavior["merge_flag"] = False
                     dependence = [dict()]
                     # for wait, output and latency
                     dependence[0]["wait"] = [[
                         0, 0, 0, key_layer_info["Infeature"],
                         key_layer_info["Inputbit"], key_layer_info["Infeature"],
-                        None, None, -1
+                        None, None, -1, None
                     ]]
                     dependence[0]["output"] = [[
                         0, 0, tile_info[2][0], tile_info[2][1],
                         key_layer_info["outputbit"], key_layer_info["Outfeature"],
-                        None, layer_id, tile_id
+                        None, layer_id, tile_id, tile_behavior["tile_id"]
                     ]]
                     # latency
                     dependence[0]["latency"] = pooling_latency
@@ -342,18 +351,19 @@ class EvaluationInterface(Component):
                 tile_behavior["layer_id"] = layer_id
                 tile_behavior["tile_id"] = merge_node_id
                 tile_behavior["target_tile_id"] = []
+                tile_behavior["source_tile_id"] = [true_id for _, _, _, true_id in tile_flag_list]
                 tile_behavior["merge_flag"] = True
                 dependence = [dict()]
                 # for wait, output and latency
                 dependence[0]["wait"] = [[
                     0, 0, start, end,
-                    key_layer_info["Inputbit"], key_layer_info["Infeature"],
-                    None, layer_id, tile_id
-                ] for start, end, tile_id in tile_flag_list]
+                    key_layer_info["outputbit"], key_layer_info["Outfeature"],
+                    None, layer_id, tile_id, true_id
+                ] for start, end, tile_id, true_id in tile_flag_list]
                 dependence[0]["output"] = [[
                     0, 0, 0, key_layer_info["Infeature"],
                     key_layer_info["outputbit"], key_layer_info["Outfeature"],
-                    None, layer_id, -1
+                    None, layer_id, -1, tile_behavior["tile_id"]
                 ]]
                 # latency
                 dependence[0]["latency"] = 1
@@ -368,6 +378,7 @@ class EvaluationInterface(Component):
                 tile_behavior["layer_id"] = layer_id
                 tile_behavior["tile_id"] = len(tile_behavior_list)
                 tile_behavior["target_tile_id"] = []
+                tile_behavior["source_tile_id"] = []
                 tile_behavior["merge_flag"] = True
                 # get pooling latency, only one PE in one Tile
                 pooling_instance = pooling_latency_analysis(
@@ -400,13 +411,13 @@ class EvaluationInterface(Component):
                         dependence[i*key_layer_info["Outputsize"][1] + j]["wait"] = [[
                             wait_row, wait_column, 0, key_layer_info["Inputchannel"], \
                             key_layer_info["Inputbit"], key_layer_info["Inputchannel"],
-                            None, None, -1
+                            None, None, -1, None
                         ]]
                         # output
                         dependence[i*key_layer_info["Outputsize"][1] + j]["output"] = [[
                             i, j, 0, key_layer_info["Outputchannel"],
                             key_layer_info["outputbit"], key_layer_info["Outputchannel"],
-                            None, layer_id, -1
+                            None, layer_id, -1, tile_behavior["tile_id"]
                         ]]
                         # latency
                         dependence[i*key_layer_info["Outputsize"][1] + j]["latency"] = float(pooling_latency)
@@ -423,7 +434,7 @@ class EvaluationInterface(Component):
                         dependence[drop_row*key_layer_info["Outputsize"][1] + drop_column]["drop"] = [[
                             i, j, 0, key_layer_info["Inputchannel"], \
                             key_layer_info["Inputbit"], key_layer_info["Inputchannel"],
-                            None, None, -1
+                            None, None, -1, None
                         ]] + origin_drop
                 tile_behavior["dependence"] = dependence
                 tile_behavior_list.append(tile_behavior)
@@ -440,6 +451,7 @@ class EvaluationInterface(Component):
                 tile_behavior["layer_id"] = layer_id
                 tile_behavior["tile_id"] = len(tile_behavior_list)
                 tile_behavior["target_tile_id"] = []
+                tile_behavior["source_tile_id"] = []
                 tile_behavior["merge_flag"] = True
                 # traverse all tiles
                 dependence = [
@@ -452,13 +464,13 @@ class EvaluationInterface(Component):
                         dependence[i*key_layer_info["Outputsize"][1] + j]["wait"] = [[
                             i, j, 0, key_layer_info["Outputchannel"], \
                             key_layer_info["Inputbit"], key_layer_info["Outputchannel"],
-                            None, None, -1
-                        ] for i in range(2)]
+                            None, None, -1, None
+                        ] for _ in range(2)]
                         # output
                         dependence[i*key_layer_info["Outputsize"][1] + j]["output"] = [[
                             i, j, 0, key_layer_info["Outputchannel"],
                             key_layer_info["outputbit"], key_layer_info["Outputchannel"],
-                            None, layer_id, -1
+                            None, layer_id, -1, tile_behavior["tile_id"]
                         ]]
                         # latency
                         dependence[i*key_layer_info["Outputsize"][1] + j]["latency"] = float(PE_latency)
@@ -470,10 +482,16 @@ class EvaluationInterface(Component):
                 tile_behavior_list.append(tile_behavior)
             else:
                 raise NotImplementedError(f"{key_layer_info['type']} is not implemented")
-        # link tile
+        # link tile, true tile id
+        def _get_true_tile_id(layer_index):
+            for tile_behavior in tile_behavior_list:
+                if tile_behavior["layer_id"] == layer_index and tile_behavior["merge_flag"]:
+                    return tile_behavior["tile_id"]
+            return None
         for layer_id, (key_layer_info, key_layer) in enumerate(zip(key_layer_info_list, key_layers)):
             # for element sum
             input_index_list = [layer_id + i for i in key_layer_info["Inputindex"]]
+            input_tile_list = [_get_true_tile_id(i) for i in input_index_list]
             target_tile_list = []
             if key_layer_info["type"] == "element_sum":
                 assert len(input_index_list) == 2
@@ -482,15 +500,25 @@ class EvaluationInterface(Component):
                 for tile_behavior in tile_behavior_list:
                     if tile_behavior["layer_id"] == layer_id and tile_behavior["merge_flag"]:
                         target_tile_list.append(tile_behavior["tile_id"])
+                        tile_behavior["source_tile_id"].append(input_tile_list[0])
+                        tile_behavior["source_tile_id"].append(input_tile_list[1])
                         for i in range(len(tile_behavior["dependence"])):
                             tile_behavior["dependence"][i]["wait"][0][7] = copy.deepcopy(input_index_list[0])
+                            tile_behavior["dependence"][i]["wait"][0][9] = copy.deepcopy(input_tile_list[0])
                             tile_behavior["dependence"][i]["wait"][1][7] = copy.deepcopy(input_index_list[1])
-                            tile_behavior["dependence"][i]["drop"][0][7] = copy.deepcopy(input_index_list[0])
-                            tile_behavior["dependence"][i]["drop"][1][7] = copy.deepcopy(input_index_list[1])
+                            tile_behavior["dependence"][i]["wait"][1][9] = copy.deepcopy(input_tile_list[1])
+                            # drop
+                            tile_behavior["dependence"][i]["drop"] = copy.deepcopy(tile_behavior["dependence"][i]["wait"])
             else:
                 # for conv, fc and pooling
                 assert len(input_index_list) == 1
                 if input_index_list[0] < 0:
+                    # for the input
+                    assert layer_id == 0
+                    assert key_layer_info["type"] == "conv"
+                    for tile_behavior in tile_behavior_list:
+                        if tile_behavior["layer_id"] == 0 and not tile_behavior["merge_flag"]:
+                            tile_behavior["source_tile_id"] = [-1]
                     continue
                 # change for input
                 for tile_behavior in tile_behavior_list:
@@ -500,15 +528,22 @@ class EvaluationInterface(Component):
                         not tile_behavior["merge_flag"]
                     ):
                         target_tile_list.append(tile_behavior["tile_id"])
+                        tile_behavior["source_tile_id"].append(input_tile_list[0])
                         for i in range(len(tile_behavior["dependence"])):
                             for wait in tile_behavior["dependence"][i]["wait"]:
                                 wait[7] = input_index_list[0]
+                                wait[9] = input_tile_list[0]
                             for drop in tile_behavior["dependence"][i]["drop"]:
                                 drop[7] = input_index_list[0]
+                                drop[9] = input_tile_list[0]
             # change for output
             for tile_behavior in tile_behavior_list:
                 if tile_behavior["layer_id"] in input_index_list and tile_behavior["merge_flag"]:
                     tile_behavior["target_tile_id"] = tile_behavior["target_tile_id"] + target_tile_list
+        # last layer
+        for tile_behavior in tile_behavior_list:
+            if tile_behavior["layer_id"] == len(key_layer_info_list) - 1 and tile_behavior["merge_flag"]:
+                tile_behavior["target_tile_id"] = [-1]
         # modify for th first fc
         for layer_id, (key_layer_info, key_layer) in enumerate(zip(key_layer_info_list, key_layers)):
             if key_layer_info["type"] == "fc":
